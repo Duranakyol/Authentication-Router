45929ab5e071bc81783221d8e18dec8234620344
Duran <80064055+Duranakyol@users.noreply.github.com>
2022-12-16 11:35:11 +0100

    Add Firebase-storage und eine Image hinzugefügt.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,6 @@
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useEffect, useState } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
 
 import { useProductListener } from "../config/firebase";
@@ -7,9 +10,46 @@ export default function Home() {
   useProductListener();
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products);
+  
+  const [url, setURL] = useState(null);
+  const imageRef = ref(storage, "photos/image-name");
+
+  useEffect(() => {
+    getDownloadURL(imageRef)
+      .then((url) => {
+        setURL(url);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [imageRef]);
+
 
   return (
     <div>
+    
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div style={{ margin: "150px 0 20px 0" }}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            const file = e.currentTarget.files[0];
+            uploadBytes(imageRef, file);
+            console.log(file);
+          }}
+        />
+      </div>
+
+      {url && <img src={url} width={240} alt="img" />}
+      
       <button
         onClick={() => {
           dispatch(addProduct());
